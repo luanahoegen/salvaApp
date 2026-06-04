b2fc68fdbfda994fff3068e02ae6a621bc7c86d5
luanahoegen <luanahoegen123@gmail.com>
2026-06-04 09:52:23 -0300

    estrutura pastas

---
+++ src/app/_layou.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack initialRouteName="login">
+        <Stack.Screen
+            name="login"
+            options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+        />
+    </Stack>
+  );
+}
